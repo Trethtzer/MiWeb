977971ade950602d6118822e8f1ac0d0891b4a9f
Trethtzer <guille@silesbonilla.com>
2020-02-19 14:09:26 +0100

    Creo el componente mainmenumovil y cambio la vista para moviles.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {CabeceraComponent} from './components/cabecera/cabecera.component';
 import {MainmenuComponent} from './components/mainmenu/mainmenu.component';
+import {MainmenumovilComponent} from './components/mainmenumovil/mainmenumovil.component';
 import {HomeComponent} from './components/home/home.component';
 import {ErrorComponent} from './components/error/error.component';
 import {ExperienceComponent} from './components/experience/experience.component';
@@ -21,6 +22,7 @@ import{ routing, appRoutingProviders} from './app.routing';
     AppComponent,
     CabeceraComponent,
     MainmenuComponent,
+    MainmenumovilComponent,
     HomeComponent,
     ErrorComponent,
     ExperienceComponent,
---
+++ src/app/components/mainmenumovil/mainmenumovil.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'mainmenumovil',
+  templateUrl: './mainmenumovil.component.html'
+})
+export class MainmenumovilComponent{
+  descripcion = 'Senior Software developer';
+}
